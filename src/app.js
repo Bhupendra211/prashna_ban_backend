6c2fa2b20407c096ce183f8123e5a3a3a104ca18
unknown <bhupendrabisht488@gmail.com>
2025-04-10 23:42:53 +0530

    update the backend api and remove redis from here

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import adminDashboard from './routes/admin/dashboard.route.js';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(helmet());
 app.use(morgan('dev'));
 dbConnect();
